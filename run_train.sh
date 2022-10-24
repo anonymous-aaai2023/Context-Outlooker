ccee4fe66bacc074476dc7192429ff4c3cc484eb
anonymous-aaai2023 <116459899+anonymous-aaai2023@users.noreply.github.com>
2022-10-24 21:23:08 +0800

    Create run_train.sh

---
+++ run_train.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+for time in {1..1}
+do
+	python3 train_squad_en.py --model_type bert\
+	    --model_name_or_path bert-base-cased\
+	    --data_dir ./data/ \
+      --architecture global-to-local\
+      --with_conv_block\
+			--output_dir ~/saved/squad2.0/bert-base\
+			--tensorboard_save_path ./runs/bert-base\
+			--train_file train-v2.0.json \
+			--predict_file dev-v2.0.json \
+			--do_train \
+			--do_eval \
+			--num_outlook_layers 2\
+			--num_train_epochs 7\
+			--evaluate_during_training \
+			--per_gpu_train_batch_size 24\
+			--per_gpu_eval_batch_size 24\
+			--version_2_with_negative \
+			--max_seq_length 384\
+
+done
