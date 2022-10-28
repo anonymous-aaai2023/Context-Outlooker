91a4a6c0d21769b3ed45f630a3e1f96f58de07af
anonymous-aaai2023 <116459899+anonymous-aaai2023@users.noreply.github.com>
2022-10-29 02:34:15 +0800

    Create run_ner.sh

---
+++ run_ner.sh
@@ -0,0 +1,10 @@
+python train_ner.py \
+    --model_name_or_path roberta-base \
+    --dataset_name conll2003 \
+    --task_name ner \
+    --max_length 128 \
+    --per_device_train_batch_size 32 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 5 \
+    --output_dir ~/saved/ner/roberta/ \
+    --gpu 0\
