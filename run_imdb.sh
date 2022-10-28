66845c515879becda0b6391278a55f6062532995
anonymous-aaai2023 <116459899+anonymous-aaai2023@users.noreply.github.com>
2022-10-29 02:41:14 +0800

    Update run_imdb.sh

--- run_imdb.sh
+++ run_imdb.sh
@@ -1,1 +1,11 @@
+#! /bin/sh
 
+python3 train_imdb_cool.py \
+    --task_name imdb\
+    --model_name_or_path roberta-base\
+    --output_dir saved/imdb/roberta-cool\
+    --num_train_epochs 7\
+    --learning_rate 2e-5 \
+    --max_length 128 \
+    --learning_rate_conv 3e-5\
+    --num_outlook_layers 2\
