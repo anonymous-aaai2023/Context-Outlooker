f2f0804676c23a5c10048d9ab3e35f450b0b2b60
anonymous-aaai2023 <116459899+anonymous-aaai2023@users.noreply.github.com>
2022-10-28 20:39:15 +0800

    Update run_qa_ms.sh

--- run_qa_ms.sh
+++ run_qa_ms.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 
-python3 train_malay.py --model_type bert \
+python3 train_squad_ms_cool.py.py --model_type bert \
         --model_name_or_path malay-huggingface/bert-base-bahasa-cased \
         --data_dir ./data_malay/ \
-    --output_dir ~/saved/malay/bert-base\
-    --tensorboard_save_path ./runs/malay-bert-base\
-    --train_file ms-train-2.0.json \
-    --predict_file ms-dev-2.0.json \
-    --do_train \
-    --do_eval \
-    --num_train_epochs 7\
-    --evaluate_during_training \
-    --learning_rate 2e-5 \
-    --per_gpu_train_batch_size 24\
-    --per_gpu_eval_batch_size 24\
-    --version_2_with_negative \
-    --max_seq_length 384
+        --output_dir ~/saved/malay/bert-base\
+        --tensorboard_save_path ./runs/malay-bert-base\
+        --train_file ms-train-2.0.json \
+        --predict_file ms-dev-2.0.json \
+        --do_train \
+        --do_eval \
+        --num_train_epochs 7\
+        --evaluate_during_training \
+        --learning_rate 2e-5 \
+        --per_gpu_train_batch_size 24\
+        --per_gpu_eval_batch_size 24\
+        --version_2_with_negative \
+        --max_seq_length 384
